34d69322b9022ba6f0ee9114556fed49519aae43
Joe Howard <joe@razeware.com>
2020-11-04 14:14:04 -0500

    11: Displaying the loading state in the Activity

--- app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesActivity.kt
+++ app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesActivity.kt
@@ -20,7 +20,9 @@ import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
@@ -61,6 +63,8 @@ class SearchRepositoriesActivity : AppCompatActivity() {
     val query = savedInstanceState?.getString(LAST_SEARCH_QUERY) ?: DEFAULT_QUERY
     search(query)
     initSearch(query)
+
+    binding.retryButton.setOnClickListener { adapter.retry() }
   }
 
   override fun onSaveInstanceState(outState: Bundle) {
@@ -73,6 +77,27 @@ class SearchRepositoriesActivity : AppCompatActivity() {
         header = ReposLoadStateAdapter { adapter.retry() },
         footer = ReposLoadStateAdapter { adapter.retry() }
     )
+    adapter.addLoadStateListener { loadState ->
+      // Only show the list if refresh succeeds.
+      binding.list.isVisible = loadState.source.refresh is LoadState.NotLoading
+      // Show loading spinner during initial load or refresh.
+      binding.progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+      // Show the retry state if initial load or refresh fails.
+      binding.retryButton.isVisible = loadState.source.refresh is LoadState.Error
+
+      // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
+      val errorState = loadState.source.append as? LoadState.Error
+          ?: loadState.source.prepend as? LoadState.Error
+          ?: loadState.append as? LoadState.Error
+          ?: loadState.prepend as? LoadState.Error
+      errorState?.let {
+        Toast.makeText(
+            this,
+            "\uD83D\uDE28 Wooops ${it.error}",
+            Toast.LENGTH_LONG
+        ).show()
+      }
+    }
   }
 
   private fun search(query: String) {
@@ -121,16 +146,6 @@ class SearchRepositoriesActivity : AppCompatActivity() {
       if (it.isNotEmpty()) {
         search(it.toString())
       }
-    }
-  }
-
-  private fun showEmptyList(show: Boolean) {
-    if (show) {
-      binding.emptyList.visibility = View.VISIBLE
-      binding.list.visibility = View.GONE
-    } else {
-      binding.emptyList.visibility = View.GONE
-      binding.list.visibility = View.VISIBLE
     }
   }
 
